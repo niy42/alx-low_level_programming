47591babaec6763467be09011696ac5ef5c37e88
niy42 <obanlaniyi42@gmail.com>
2022-06-30 15:27:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * file:4-puts.c
+ * author: Obanlan Adeniyi
+ */
+#include <stdio.h>
+/*
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (sucess)
+ */
+ * int main(void)
+ {
+ 	puts("\"progr amming is like building a multilingual puzzle");
+ 	return (0);
+ }
