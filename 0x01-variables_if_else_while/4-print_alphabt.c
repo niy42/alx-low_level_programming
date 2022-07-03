6502ef026e1a4a1db1b16fbda72994fb874bc7e6
niy42 <obanlaniyi42@gmail.com>
2022-07-03 14:52:36 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* 4-print_alphabet.c */
+
+#include <stdio.h>
+
+/*  a program that prints the alphabet in lowercase except e and q */
+
+/**
+ * main - Entry point
+ * Author: Obanla Adeniyi
+ * Description: 'a program that prints the alphabet in lowercase e and q'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	putchar('\n')
+	}
+
+	return (0);
+
+}
