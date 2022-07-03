ec8f670f1638a75b65ca181ae69594528ffd7c12
niy42 <obanlaniyi42@gmail.com>
2022-07-03 12:30:34 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -21,10 +21,8 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
 	putchar('\n');
-
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	putchar(letter);
-	putchar('\n');
 
 	return (0);
 
