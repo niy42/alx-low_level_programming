e9ff7a2d5ce81b9b2cbfcd4b54d8e01e31bb582a
niy42 <obanlaniyi42@gmail.com>
2022-07-03 14:22:09 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -20,10 +20,8 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
-	
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	putchar(letter);
-	
 	putchar('\n');
 
 	return (0);
