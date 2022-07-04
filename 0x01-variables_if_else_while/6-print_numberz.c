9cc37ab2cb5ce7d5c0a65a5bbeed810a95c59bcd
niy42 <obanlaniyi42@gmail.com>
2022-07-04 12:12:25 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* 6-print_numberz.c */
+
+#include <stdio.h>
+
+/*a program that prints all single digit numbers of base 10 starting from 0*/
+
+/**
+ * main - Entry point
+ * Author: Obanla Adeniyi
+ * Description: 'prints all single digit numbers of base 10 starting from 0'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	putchar((num % 10) + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
