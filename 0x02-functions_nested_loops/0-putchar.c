1e084bbe78c22018836d9e1221ee39bb2ece0f39
niy42 <obanlaniyi42@gmail.com>
2022-07-05 11:29:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
-int main()
-{
-	printf("_putchar\n");
-	
-	return 0;
+/**
+ * main - prints _putchar
+ * description: 'A program to print _putchar'*
+ * Return: Always
+*/
 
+int main(void)
+{
+	char X[] = "_putchar";
+
+	printf("%s\n", X);
+	return (0);
 }
