657aeb6642bb3a5a53106ac763fb46e35ef3c8e4
niy42 <obanlaniyi42@gmail.com>
2022-07-11 09:18:53 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,7 @@
 */
 int print_last_digit(int c)
 {
-	int last_digit = c % 10;
-
-	_putchar(last_digit + '0');
-	_putchar('\n');
-	return (last_digit);
+	c = c % 10;
+	_putchar(c + '0');
+	return (c);
 }
