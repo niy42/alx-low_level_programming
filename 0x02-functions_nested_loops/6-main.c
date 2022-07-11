8e0a94ce86092b09f46a855039459635de3cd9ce
niy42 <obanlaniyi42@gmail.com>
2022-07-11 05:00:10 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*main - entry point
+*
+*Return: 0
+*/
+int main(void)
+{
+	int _abs(int);
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,6 @@ int _putchar(char c);
 void printputchar(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
+int _abs(int n);
+
 #endif
