8b336ade6bb3f70ae0ed9541dea8a000deb38da7
niy42 <obanlaniyi42@gmail.com>
2022-07-25 11:43:12 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return length of string
+ *
+ * @s: string to get length of
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
