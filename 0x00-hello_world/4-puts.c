16c17aa75ef2fa47fa0aabc6c2385a82ade15cc1
niy42 <obanlaniyi42@gmail>
2022-10-13 07:28:37 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*Main - "Programming is like building a 
+*	multilingual puzzle, followed by a newline"
+*
+*Return: Alaways(0)
+*/
+int main(void)
+{
+	puts("programming is like building a mulitilingual puzzle\n");
+	return(0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This project is about 0x00. C - Hello, World
