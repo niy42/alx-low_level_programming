121b6169926256713a771814a57f23b0ad771207
niy42 <obanlaniyi42@gmail>
2022-10-14 13:59:14 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,40 @@
-/*
- * File: 1-last_digit.c
- *
- * Author: Obanla Adeniyi
- */
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+
+/*1-last_digit.c*/
+
+
+#include <stdio.h>
+
+
+/*prints the value of the last digit of n*/
+
 /**
- * main - Prints last digit of a number and states
- *	 whether greater than 5, equal to 0, 0r less than 6 and not zero
- *
- * Return: Always 0.
+ * main - Entry point
+ * Author: Obanla Adeniyi
+ * Description: 'prints the value of the last digit of n'
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int Num;
+	int n;
 
 	srand(time(0));
-	Num = rand() - RAND_MAX / 2;
-
-	if ((Num % 10) > 5)
-	printf("Last digit is %d and is greater than 5 \n", (Num % 10));
-
-	else if ((Num % 10) < 6 && (Num % 10) != 0)
-	printf("The last digit is %d is less than 6 and not 0 \n", (Num % 10));
-
+	n = rand() - RAND_MAX / 2;
+	if ((n % 10) > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
 	else
-	printf("%d is the last digit and is 0 \n", (Num % 10));
-
+	{
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
 	return (0);
 }
+
