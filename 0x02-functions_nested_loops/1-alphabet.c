894251a67ec154fa36a44c2f2920fea6187c87d6
niy42 <obanlaniyi42@gmail>
2022-10-18 11:10:33 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,10 +10,10 @@ void print_alphabet(void)
 	char y;
 
 	{
-		for (y = 'a'; y <= 'z'; y++)
-			{
-				_putchar(y);
-			}
-		_putchar('\n');
+	for (y = 'a'; y <= 'z'; y++)
+		{
+		_putchar(y);
+		}
+	_putchar('\n');
 	}
 }
