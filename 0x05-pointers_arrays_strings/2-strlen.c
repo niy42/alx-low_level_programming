899843964d47b55f3e66ca69a10c0352df9acea6
niy42 <obanlaniyi42@gmail>
2022-10-25 14:20:15 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @s: The string to get the length of.
  *
  * Return: The length of @str.
  */
