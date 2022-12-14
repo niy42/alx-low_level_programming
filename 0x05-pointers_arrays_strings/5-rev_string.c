8cd856c76c17d559b83e7af583cc60f69aa92777
niy42 <obanlaniyi42@gmail>
2022-12-14 05:35:35 -0800

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+void rev_string(char *s)
+{
+	int i = 0, j;
+	char tmp;
+	
+	while (s[i] != '\0')
+		i++;
+	i--;
+	for (j = 0; j <=  i/2; j++)
+	{		
+		tmp = s[j];
+		s[j] = s[i - j];
+		s[i - j] = tmp;
+	}
+}
