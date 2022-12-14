72c23a21953d1d0cf1167636bfac742937385ecd
niy42 <obanlaniyi42@gmail>
2022-12-14 06:41:57 -0800

    pointer_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,16 @@
 #include "main.h"
+/**
+ * puts2 - prints only even indexed characters from the argument
+ * @str: character to be printed
+ *
+ */
 void puts2(char *str)
 {
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if(i % 2 == 0)
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
