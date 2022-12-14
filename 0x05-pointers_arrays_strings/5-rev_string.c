c07839f88ee8c87dad3b93cb299080fcbd1f9504
niy42 <obanlaniyi42@gmail>
2022-12-14 05:43:35 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,18 @@
+/**
+ * rev_string - prints string in reverse
+ * @s: character to be printed out
+ */
+
 void rev_string(char *s)
 {
 	int i = 0, j;
 	char tmp;
-	
+
 	while (s[i] != '\0')
 		i++;
 	i--;
-	for (j = 0; j <=  i/2; j++)
-	{		
+	for (j = 0; j <=  i / 2; j++)
+	{
 		tmp = s[j];
 		s[j] = s[i - j];
 		s[i - j] = tmp;
