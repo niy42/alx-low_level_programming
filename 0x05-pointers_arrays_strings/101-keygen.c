043c66cb05bee0a5c4106783e89cc2552545a4a5
niy42 <obanlaniyi42@gmail>
2023-04-24 13:08:00 -0700

    Modified password gen for crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,74 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<unistd.h>
-/**
- * _strlen - counts each element in the string array
- * @s: characters to be counted
- * Return: length of string
+/*
+ * File: 101-keygen.c
+ *
  */
-int _strlen(char *s)
-{
-	int i;
-	size_t index;
 
-	i = 0;
-	index = 0;
-	do {
-		index += 1;
-		i = i + 1;
-	} while (*(s + index) != '\0');
-	return (index);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- * rand_password - generates random character password
- * @length: length of the password generated
- * Return: Tada! Congrats
- */
-char rand_password(int length)
-{
-	char *r = "Tada! Congrats";
-	char *password = malloc(length * sizeof(char));
-	int i;
-	char *number = "0123456789";
-	int number_length = _strlen(number);
-	char *upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
-	int upper_length = _strlen(upper);
-	char *lower = "qwertyuiopasdfghjklzxcvbnm";
-	int lower_length = _strlen(lower);
-	char *symbol = "!@#$%^&*?+=-";
-	int symbol_length = _strlen(symbol);
-
-	srand(time(NULL) * getpid());
-	i = 0;
-	for (; i < length; i++)
-	{
-		int randomiser = rand() % 4;
-
-		if (randomiser == 0)
-			password[i] = *(number + (rand() % number_length));
-		else if (randomiser == 1)
-			*(password + i) = lower[rand() % lower_length];
-		else if (randomiser == 2)
-			password[i] = symbol[rand() % symbol_length];
-		else if (randomiser == 3)
-			*(password + i) = *(upper + (rand() % upper_length));
-		/*printf("%c", password[i]);*/
-	}
-	free(password);
-	if (password)
-		printf("%s", r);
-	printf("\n");
-	return (*r);
-}
-/**
- * main - calls rand_password function
- * Return: Always zero (success)
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int length = 10;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	rand_password(length);
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s\n", password);
+
 	return (0);
 }
