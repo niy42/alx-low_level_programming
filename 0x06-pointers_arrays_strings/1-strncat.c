d51c0cdbf39249e09944c962857bc40c320e6f5d
niy42 <obanlaniyi42@gmail.com>
2023-07-03 07:34:10 -0700

    appends src ntimes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * _strncat - appends limited source characters to destination
+ * @src: source string 
+ * @dest: destination string 
+ * @n: limited characters of src string to be appended to dest
+ * Return: dest string
+ */
+ char *_strncat(char *dest, char *src, int n)
+ {
+	int i;
+	char *tmp;
+
+	i = 0;
+	if (dest == NULL)
+	{
+		fprintf(stderr, "error - no memory, unable to append");
+	}
+	else
+	{
+		tmp = dest;
+		do {
+			dest++;
+		} while (*dest);
+		while (src[i] && i < n)
+		{
+			*dest = src[i];
+			dest++;
+			++i;
+		}
+	}
+	dest = '\0';
+	return (tmp);
+ }
